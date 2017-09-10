ffcb1f9034f07c6646d84e462f17a0bd2bc56a09
Patrick Martin <patrickmmartin@gmail.com>
2017-09-10 13:42:33 +0100

    update ignores; restore demo file

--- Sustain Tests/Reseat Neck/examples/wire3d_demo.py
+++ Sustain Tests/Reseat Neck/examples/wire3d_demo.py
@@ -16,8 +16,6 @@ ax = fig.add_subplot(111, projection='3d')
 # Grab some test data.
 X, Y, Z = axes3d.get_test_data(0.5)
 
-print "X: {0}\nY: {1}\nZ: {2}".format(X, Y, Z) 
-
 # Plot a basic wireframe.
 ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1)
 
